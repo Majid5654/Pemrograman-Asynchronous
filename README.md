951bfad6c7ed3661d5b6ec5f1897288e5730ecda
Majid5654 <3rwanmajid@gmail.com>
2025-11-13 16:56:28 +0700

    w11:soal 3

--- README.md
+++ README.md
@@ -1,5 +1,33 @@
 # Erwan Majid 06
 
-# Menampilkan data json
+# Soal 1
+
+Tambahkan nama panggilan Anda pada title app sebagai identitas hasil
+pekerjaan Anda
+
+![json](IMG/2.png)
+
+# Soal 2
+
+cobalah akses di browser URI tersebut dengan lengkap seperti ini. Jika menampilkan data JSON, maka Anda telah berhasi
 
 ![json](IMG/1.png)
+
+# Soal 3
+
+Jelaskan maksud kode langkah 5 tersebut terkait substring dan catchError!
+
+-substring : Untuk mencegah tampilan teks terlalu panjang di layar saat menampilkan hasil dari API.
+Karena hasil Google Books API biasanya sangat panjang (ratusan baris JSON), jadi cukup ditampilkan sebagian
+
+-catchError : digunakan untuk menangani error jika proses getData() gagal — misalnya:
+Tidak ada koneksi internet, URL salah,Server API down
+
+-setState : digunakan untuk memperbarui tampilan (UI) setiap kali nilai result berubah — baik saat sukses maupun error. Flutter akan me-render ulang Text(result) di layar dengan isi terbaru
+
+substring(0, 450) membatasi teks dari hasil API supaya tidak terlalu panjang.
+catchError menangkap dan menampilkan pesan error jika permintaan API gagal
+
+# Result :
+
+![result](IMG/2.gif)
--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Future Demo',
+      title: 'Erwan Majid 06',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
@@ -43,12 +43,26 @@ class _FuturePageState extends State<FuturePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Back from the Future')),
+      appBar: AppBar(title: const Text('Erwan Majid 06 - Back from the Future')),
       body: Center(
         child: Column(
           children: [
             const Spacer(),
-            ElevatedButton(child: const Text('GO!'), onPressed: () {}),
+            ElevatedButton(
+              child: Text('GO!'),
+              onPressed: () {
+                setState(() {});
+                getData()
+                    .then((value) {
+                      result = value.body.toString().substring(0, 450);
+                      setState(() {});
+                    })
+                    .catchError((_) {
+                      result = 'An error occurred';
+                      setState(() {});
+                    });
+              },
+            ),
             const Spacer(),
             Text(result),
             const Spacer(),
